06dac79bc5210a15b4b494d13b22b6553e840f47
Gustav <gusan092@student.liu.se>
2018-11-16 22:16:58 +0100

    Player sprite

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pygame
+import sprites
+
+WINDOW_SIZE = (1600,900)
 
 def main():
     pygame.init()
 
-    WINDOW_SIZE = (1024,768)
     screen = pygame.display.set_mode(WINDOW_SIZE)
-
+    player = sprites.Player()
+    player.rect.x = 40
+    player.rect.y = int(WINDOW_SIZE[1] / 2 + sprites.PLAYER_SIZE[1] / 2)
+    all = pygame.sprite.Group()
+    all.add(player)
     running = True  # True as long as the game should be running
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+
+        all.draw(screen)
+        pygame.display.flip()
+
 
 main()
--- sprites.py
+++ sprites.py
@@ -1,7 +1,15 @@
 import pygame
 
+PLAYER_SIZE = (40, 20)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([PLAYER_SIZE[0], PLAYER_SIZE[1]])
+        self.image.fill((255,255,255))
+
+        self.rect = self.image.get_rect()
 
 class Obstacles(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__(self)
