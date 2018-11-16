d71c481ebf5db94052d1f856788cc6da9f87c18c
Gustav <gusan092@student.liu.se>
2018-11-17 00:01:25 +0100

    Merge branch 'master' of https://gitlab.ida.liu.se/teoga849/FallGameJam2018
    # Conflicts:
    #	main.py

--- sprites.py
+++ sprites.py
@@ -12,6 +12,21 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.lane = 2
 
+class ScrollObjects(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__(self)
+
+    def update(self, *args):
+        for s in self.sprites():
+            s.rect.x -= 1 # TODO: Access scroll_length
+            if s.rect.x + s.rect.width <= 0:
+                s.kill()
+
 class Obstacles(pygame.sprite.Group):
     def __init__(self):
         super().__init__(self)
+
+    def update(self, *args):
+        for s in self.sprites():
+            return
+
