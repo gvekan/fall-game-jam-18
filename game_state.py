65d0bda531fc6c14637c14d2afa66686d9ce831c
Gustav <gusan092@student.liu.se>
2018-11-17 10:46:58 +0100

    Merge branch 'master' of https://gitlab.ida.liu.se/teoga849/FallGameJam2018
    # Conflicts:
    #	main.py
    #	sprites.py

--- game_state.py
+++ game_state.py
@@ -2,6 +2,7 @@ class State:
     def __init__(self):
         self.era = 2
         self.scroll_length = 1
+        self.kill = set()
 
 
 state = State()
--- sprites.py
+++ sprites.py
@@ -2,6 +2,15 @@ import pygame
 from constants import *
 from game_state import state
 
+class All(pygame.sprite.Group):
+    def __init__(self):
+        pygame.sprite.Group.__init__(self)
+
+    def update(self, *args):
+        for s in self.sprites():
+            s.update()
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -11,15 +20,17 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.lane = 2
 
+
 class ScrollObjects(pygame.sprite.Group):
     def __init__(self):
         super().__init__(self)
 
-    def update(self, *args):
+    def update(self, state, *args):
         for s in self.sprites():
             s.rect.x -= state.scroll_length
             if s.rect.x + s.rect.width <= 0:
-                s.kill()
+                state.kill.add(s)
+
 
 class Obstacles(pygame.sprite.Group):
     def __init__(self):
@@ -30,4 +41,19 @@ class Obstacles(pygame.sprite.Group):
             # TODO: Check collision
             return
 
+
+class Animation:
+    def __init__(self, images, speed):
+        self.index = 0
+        self.images = images
+        self.speed = speed
+        self.count = 0
+
+    def update(self, sprite: pygame.sprite.Sprite):
+        self.count += 1
+        if self.count == self.speed:
+            self.index += 1
+            if self.index == len(self.images):
+                self.index = 0
+            sprite.image = self.images[self.index]
 
